5fefddc23b33e017f514f1484b1dca4c12c62b7c
akashkayala <134595277+akashkayala@users.noreply.github.com>
2024-02-10 02:45:53 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,113 @@
+# AI Thermometer
+Contactless temperature mesurement using IR & RGB cameras and Deep CNN facial detection.
+
+### Normal temperature
+![](docs/images/retinaface/healthy.gif)
+### Elevated temperature
+![](docs/images/retinaface/elevated.gif)
+
+## What's new in v0.4
+* New facial detector (handles partial occlusion, e.g. with a mask)
+* Added IR camera drift compensation with an external blackbody
+
+## Hardware 
+To build this project you will need:
+|#|Part|link|Price (USD)|
+|-|-|-|-|
+|1|Jetson Nano Dev Kit|[link](https://www.sparkfun.com/products/16271)| 99 | 
+|3|FLIR Lepton 3.5 IR Camera | [link](https://store.groupgets.com/products/flir-lepton-3-5)| 199 |
+|4|GroupGets Purethermal2 Module | [link](https://store.groupgets.com/products/purethermal-2)| 99 |
+|2|Raspberry Pi Camera Module V2.1 |[link](https://www.sparkfun.com/products/14028) | 25 |
+|5|Noctua cooling fan | [link](https://www.amazon.com/Noctua-NF-A4x10-PWM-4-Pin-Premium/dp/B07DXRNYNX/) | 14 |
+|6|3D printed enclosure | [https://a360.co/3nmDML3](https://a360.co/3nmDML3) | - |
+|total | | | 436 |
+
+Enclosure (live model available from [https://a360.co/3nmDML3](https://a360.co/3nmDML3) ): 
+![wireframe image](docs/images/wireframe.png)
+
+## Quickstart 
+
+1. Flash Nvidia Jetson Nano with the latest Nvidia JetPack. Update package manager
+
+```shell
+sudo apt update && sudo apt upgrade
+```
+
+2. Build OpenCV >4.4. It is required for CUDA DNN support.
+```shell
+wget https://raw.githubusercontent.com/mdegans/nano_build_opencv/master/build_opencv.sh
+chmod +x build_opencv.sh
+./build_opencv.sh
+```
+3. Build pytorch (instructions [here](https://forums.developer.nvidia.com/t/pytorch-for-jetson-version-1-7-0-now-available/72048))
+
+4. Download AI Thermometer
+
+```shell
+git clone --recursive https://github.com/tomek-l/ai-thermometer
+cd ai-thermometer
+```
+
+5. Run AI Thermometer
+
+```shell
+python3 main.py
+```
+
+6. Optionally, 3D print & assemble the enclosure from [https://a360.co/3nmDML3](https://a360.co/3nmDML3)
+
+## Limitations
+
+### IR and RGB camera alignment.
+
+The current way of calculating the correspondence between IR and RGB cameras is not ideal.
+Factors, such as the non-rigid mount of the sensor on the Raspberry Pi CMV2.1 don't help.
+I'm actively working on calibration code that takes into account the intrinsic parameters of both cameras, which should allow for obtaining a pixel-level correspondence between the imags.
+
+## FAQ/common issues:
+
+1. `uvc_open error -3`
+
+Reason: Your current user does not have r/w access to the PureThermal USB device. 
+
+For a quick and dirty fix you can do do:
+```shell
+echo 'SUBSYSTEM=="usb",  ENV{DEVTYPE}=="usb_device", GROUP="plugdev", MODE="0664"' | sudo tee /etc/udev/rules.d/10-libuvc.rules 
+sudo udevadm trigger
+```
+(this gives camera access to all users)
+
+For a better, fine-grained permission setup, create a new group, give permissions to that group only and your user to the newly created group. You can read more [here](http://wiki.ros.org/libuvc_camera#Permissions).
+
+2. `Illegal instruction (core dumped)`
+
+Reason: The provided  ```libuvc.so``` is compiled for AArch64.
+It is a custom version of libuvc that supports Y16 video format. If you're using different architecture, you will need to build the library from source:
+
+```shell
+git clone https://github.com/groupgets/libuvc
+cd libuvc
+mkdir build
+cd build
+cmake ..
+make
+cp libuvc.so ~/ai-thermometer/ir/libuvc_wrapper
+```
+
+3. using sudo or getting `permission denied`
+
+You don't need ```sudo``` to run the code in this repository 🙂
+
+## Previous versions
+
+## Version 0.2: Haar Cascade + FLIR Lepton 3.5 temp. measurement (~10FPS, CPU only)
+Watch full video: [https://www.youtube.com/watch?v=j9eo9Cs8J8I](https://www.youtube.com/watch?v=j9eo9Cs8J8I)
+
+![](docs/images/haar/combined_short.gif)
+
+
+## Version 0.1: SSD (COCO) + FLIR Lepton 3.5 temp. measurement (~12FPS w/ GPU accel.)
+Watch full video: [https://www.youtube.com/watch?v=i2XMtshdjn8](https://www.youtube.com/watch?v=i2XMtshdjn8)
+
+![](docs/images/ssd/ssd_short.gif)
+
---
+++ ir/purethermal.py
@@ -0,0 +1,224 @@
+from threading import Thread
+from queue import Queue
+import numpy as np
+import cv2, time
+
+# IR camera
+from .libuvc_wrapper import *
+from .utils import ktoc, resize, normalize, crop_telemetry, detect_ir, drop_small_bboxes
+
+
+def uvc_init(ctx):
+    res = libuvc.uvc_init(byref(ctx), 0)
+    if res < 0:
+        print("uvc_init error")
+        exit(res)
+
+
+def find_device(ctx, dev):
+    res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
+    if res < 0:
+        print("uvc_find_device error")
+        exit(res)
+
+
+def open_device(dev, devh):
+    res = libuvc.uvc_open(dev, byref(devh))
+    if res < 0:
+        print(f"uvc_open error {res}")
+        exit(res)
+    else:
+        print("device opened!")
+
+
+def check_frame_formats(frame_formats):
+    if len(frame_formats) == 0:
+        print("device does not support Y16")
+        exit(1)
+
+
+def start_streaming(devh, ctrl, ptr_callback):
+    res = libuvc.uvc_start_streaming(devh, byref(ctrl), ptr_callback, None, 0)
+    if res < 0:
+        print("uvc_start_streaming failed: {0}".format(res))
+        exit(1)
+
+
+def start_pt2(dev, devh, ctx, q):
+    # initialize Pure Thermal 2 board
+    ctrl = uvc_stream_ctrl()
+
+    uvc_init(ctx)
+    find_device(ctx, dev)
+    open_device(dev, devh)
+
+    print_device_info(devh)
+    print_device_formats(devh)
+
+    frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+    check_frame_formats(frame_formats)
+
+    libuvc.uvc_get_stream_ctrl_format_size(
+        devh,
+        byref(ctrl),
+        UVC_FRAME_FORMAT_Y16,
+        frame_formats[0].wWidth,
+        frame_formats[0].wHeight,
+        int(1e7 / frame_formats[0].dwDefaultFrameInterval),
+    )
+
+    PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(
+        py_frame_callback
+    )
+    start_streaming(devh, ctrl, PTR_PY_FRAME_CALLBACK)
+
+    return PTR_PY_FRAME_CALLBACK
+
+
+def py_frame_callback(frame, userptr):
+    array_pointer = cast(
+        frame.contents.data,
+        POINTER(c_uint16 * (frame.contents.width * frame.contents.height)),
+    )
+    data = np.frombuffer(array_pointer.contents, dtype=np.dtype(np.uint16)).reshape(
+        frame.contents.height, frame.contents.width
+    )
+
+    assert frame.contents.data_bytes == (
+        2 * frame.contents.width * frame.contents.height
+    )
+
+    if not q.full():
+        q.put(data)
+
+
+def setup():
+    ctx = POINTER(uvc_context)()
+    dev = POINTER(uvc_device)()
+    devh = POINTER(uvc_device_handle)()
+    ctrl = uvc_stream_ctrl()
+
+    res = libuvc.uvc_init(byref(ctx), 0)
+    if res < 0:
+        print("uvc_init error")
+        exit(res)
+
+    res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
+    if res < 0:
+        print("uvc_find_device error")
+        exit(res)
+
+    res = libuvc.uvc_open(dev, byref(devh))
+    if res < 0:
+        print(f"uvc_open error {res}")
+        exit(res)
+
+    print("device opened!")
+
+    print_device_info(devh)
+    print_device_formats(devh)
+
+    frame_formats = uvc_get_frame_formats_by_guid(devh, VS_FMT_GUID_Y16)
+    if len(frame_formats) == 0:
+        print("device does not support Y16")
+        exit(1)
+
+    libuvc.uvc_get_stream_ctrl_format_size(
+        devh,
+        byref(ctrl),
+        UVC_FRAME_FORMAT_Y16,
+        frame_formats[0].wWidth,
+        frame_formats[0].wHeight,
+        int(1e7 / frame_formats[0].dwDefaultFrameInterval),
+    )
+
+    PTR_PY_FRAME_CALLBACK = CFUNCTYPE(None, POINTER(uvc_frame), c_void_p)(
+        py_frame_callback
+    )
+
+    res = libuvc.uvc_start_streaming(devh, byref(ctrl), PTR_PY_FRAME_CALLBACK, None, 0)
+    if res < 0:
+        print("uvc_start_streaming failed: {0}".format(res))
+        exit(1)
+
+    return ctx, dev, devh, ctrl
+
+
+class IRThread(Thread):
+    def __init__(self, bufsize=2, thr_temp=28):
+        super(IRThread, self).__init__()
+
+        self._ctx = POINTER(uvc_context)()
+        self._dev = POINTER(uvc_device)()
+        self._devh = POINTER(uvc_device_handle)()
+        self._cb_ptr = start_pt2(self._dev, self._devh, self._ctx, q)
+
+        self._thr_temp = thr_temp
+        self._raw = None
+        self._arr_deg_c = None
+
+        self._latency = -1.0
+        self._running = True
+
+    def run(self):
+        try:
+            while self._running:
+                start_time = time.monotonic()
+
+                raw = q.get(True, 500)
+                raw = crop_telemetry(raw)
+                arr_c = ktoc(raw.astype(np.float32))  # Convert 16-bit Kelvin to deg C.
+                # Casting to float prevents underflow of uint16 type in ktoc between -10C to 0C
+
+                # Save memebers
+                self._raw = raw
+                self._arr_deg_c = arr_c
+                self._latency = 1000 * (time.monotonic() - start_time)
+
+        finally:
+            self._exit_handler()
+
+    def _exit_handler(self):
+        libuvc.uvc_stop_streaming(self._devh)
+        libuvc.uvc_unref_device(self._dev)
+        libuvc.uvc_exit(self._ctx)
+
+    @property
+    def raw(self):
+        return self._raw.copy()
+
+    @property
+    def temps(self):
+        # TODO: locking much?
+        return self._arr_deg_c
+
+    @property
+    def latency(self):
+        return self._latency
+
+    def stop(self):
+        self._running = False
+
+
+q = Queue(
+    2
+)  # not ideal, but q must be global for the callback function to have access to it
+
+if __name__ == "__main__":
+
+    # example usage
+
+    ctx = POINTER(uvc_context)()
+    dev = POINTER(uvc_device)()
+    devh = POINTER(uvc_device_handle)()
+
+    p = start_pt2(dev, devh, ctx, q)
+
+    try:
+        while True:
+            data = q.get(True, 500)
+            print(ktoc(data))
+    finally:
+        libuvc.uvc_stop_streaming(devh)
+        libuvc.uvc_unref_device(dev)
+        libuvc.uvc_exit(ctx)
